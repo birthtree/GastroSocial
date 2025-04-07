99347af2b8d49102875e51db89d0986be8ea45df
birthtree <vladimirakkuzin10@gmail.com>
2025-04-07 18:04:36 +0300

    Start project

---
+++ src/test/java/ru/akkuzin/vkr/backendVKR/BackendVkrApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akkuzin.vkr.backendVKR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
